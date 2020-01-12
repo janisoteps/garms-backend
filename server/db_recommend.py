d7f003b043736ee0626f3e0e70a38495e37a6c9b
Janis Dzikevics <jdzikevics@zmags.com>
2020-01-12 23:39:59 +0100

    fix recommender

--- server/db_recommend.py
+++ server/db_recommend.py
@@ -2,6 +2,7 @@ from sqlalchemy import func, any_, and_, or_
 from marshmallow_schema import ProductsSchema, ImageSchema, ProductSchemaV2
 import json
 import data.cats as cats
+from random import shuffle
 
 
 def recommend_similar_tags(db, User, ProductsV2, data):
@@ -18,48 +19,41 @@ def recommend_similar_tags(db, User, ProductsV2, data):
         kind_conditions = []
 
         for look in user_looks:
+            print(look)
             outfits = [outfit['prod_id'] for outfit in user_wardrobe if outfit['look_name'] == look['look_name']]
-            # print('outfits')
-            # print(outfits)
             if len(outfits) > 0:
-                # print('getting user outfits')
                 query_prods = db.session.query(ProductsV2).filter(ProductsV2.prod_id.in_(outfits)).all()
-                # print('loaded user outfits')
                 look_cats_kind = []
-                look_cats_all = []
                 for query_prod in query_prods:
-                    query_name = query_prod.name
-                    # print(f'query_name={query_name}')
-                    for query_word in query_name.split(' '):
-                        # print(f'query_word={query_word}')
-                        if query_word.lower() in cats.Cats().kind_cats and query_word.lower() not in look_cats_kind:
-                            # print(f'added to look_cats_kind: {query_word.lower()}')
-                            look_cats_kind.append(query_word.lower())
-                        if query_word.lower() in cats.Cats().all_cats and query_word.lower() not in look_cats_all:
-                            # print(f'added to look_cats_all: {query_word.lower()}')
-                            look_cats_all.append(query_word.lower())
+                    print(query_prod.name)
+                    query_kind_cats = query_prod.kind_cats
+                    for query_kind_cat in query_kind_cats:
+                        if query_kind_cat not in look_cats_kind:
+                            look_cats_kind.append(query_kind_cat)
 
-                for tag in look_cats_kind:
+                print(look_cats_kind)
+                for cat in look_cats_kind:
                     kind_conditions.append(
-                        ProductsV2.kind_cats.any(tag)
+                        (ProductsV2.kind_cats.any(cat))
                     )
 
-        print('querying for recommended prods')
-        query = db.session.query(ProductsV2).filter(
-            and_(or_(*kind_conditions), (ProductsV2.sex == req_sex), ProductsV2.prod_id.isnot(None))
-        )
+            print('querying for recommended prods')
+            query = db.session.query(ProductsV2).filter(
+                and_(or_(*kind_conditions), (ProductsV2.sex == req_sex), ProductsV2.prod_id.isnot(None))
+            )
 
-        query_results = query.order_by(func.random()).limit(30).all()
-        print('loaded recommended prods')
-        prod_results = []
-        for query_result in query_results:
-            prod_serial = ProductSchemaV2().dump(query_result)
-            prod_results.append(prod_serial)
+            query_results = query.order_by(func.random()).limit(10).all()
+            print('loaded recommended prods')
+            # prod_results = []
+            for query_result in query_results:
+                prod_serial = ProductSchemaV2().dump(query_result)
+                # prod_results.append(prod_serial)
 
-        suggestions.append({
-            'look_name': look['look_name'],
-            'prod_suggestions': prod_results
-        })
+                suggestions.append({
+                    'look_name': look['look_name'],
+                    'prod_suggestions': [prod_serial]
+                })
+
     else:
         query = db.session.query(ProductsV2).filter(ProductsV2.prod_id.isnot(None)).filter(ProductsV2.sex == req_sex)
         query_results = query.order_by(func.random()).limit(30).all()
@@ -72,6 +66,7 @@ def recommend_similar_tags(db, User, ProductsV2, data):
             'prod_suggestions': prod_results
         })
 
+    shuffle(suggestions)
     return json.dumps(suggestions)
 
 
