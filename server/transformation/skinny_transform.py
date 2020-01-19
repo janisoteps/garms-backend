a62912aea1fc2aad3d963399bf730d9fae7918d8
Janis Dzikevics <jdzikevics@zmags.com>
2020-01-19 22:33:50 +0100

    update query reading

--- server/transformation/skinny_transform.py
+++ server/transformation/skinny_transform.py
@@ -20,33 +20,33 @@ class SkinnyTransform:
                 query_result = ImagesV2.query.filter_by(img_hash=img_hash).first()
 
                 discount_rate = 0
-                if query_result['sale'] == True:
-                    discount_rate = (query_result['price'] - query_result['saleprice']) / query_result['price']
+                if query_result.sale == True:
+                    discount_rate = (query_result.price - query_result.saleprice) / query_result.price
 
                 commit_dict = {
-                    'img_hash': query_result['img_hash'],
-                    'img_url': query_result['img_url'],
-                    'prod_id': query_result['prod_id'],
-                    'prod_url': query_result['prod_url'],
-                    'brand': query_result['brand'],
-                    'color_string': query_result['color_string'],
-                    'date':  query_result['date'],
-                    'name': query_result['name'],
-                    'price': query_result['price'],
-                    'sale': query_result['sale'],
-                    'saleprice': query_result['saleprice'],
+                    'img_hash': query_result.img_hash,
+                    'img_url': query_result.img_url,
+                    'prod_id': query_result.prod_id,
+                    'prod_url': query_result.prod_url,
+                    'brand': query_result.brand,
+                    'color_string': query_result.color_string,
+                    'date':  query_result.date,
+                    'name': query_result.name,
+                    'price': query_result.price,
+                    'sale': query_result.sale,
+                    'saleprice': query_result.saleprice,
                     'discount_rate': discount_rate,
-                    'sex': query_result['sex'],
-                    'shop': query_result['shop'],
-                    'kind_cats': query_result['kind_cats'],
-                    'color_pattern_cats': query_result['color_pattern_cats'],
-                    'style_cats': query_result['style_cats'],
-                    'material_cats': query_result['material_cats'],
-                    'attribute_cats': query_result['attribute_cats'],
-                    'filter_cats': query_result['filter_cats'],
-                    'all_cats': query_result['all_cats'],
-                    'size_stock': query_result['size_stock'],
-                    'in_stock': query_result['in_stock']
+                    'sex': query_result.sex,
+                    'shop': query_result.shop,
+                    'kind_cats': query_result.kind_cats,
+                    'color_pattern_cats': query_result.color_pattern_cats,
+                    'style_cats': query_result.style_cats,
+                    'material_cats': query_result.material_cats,
+                    'attribute_cats': query_result.attribute_cats,
+                    'filter_cats': query_result.filter_cats,
+                    'all_cats': query_result.all_cats,
+                    'size_stock': query_result.size_stock,
+                    'in_stock': query_result.in_stock
                 }
 
                 image_commit_v2_skinny(db, ImagesV2Skinny, commit_dict)
