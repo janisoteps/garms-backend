0d96b722290e5d8d3ffb02c1c3abf1d85c484a5a
Janis Dzikevics <jdzikevics@zmags.com>
2020-01-23 19:32:32 +0100

    update endpoints

--- server/endpoints.py
+++ server/endpoints.py
@@ -1,8 +1,8 @@
 # endpoints.py
 
-CAT_API_PATH = 'http://34.245.0.175/api/cats'  # when running locally
-# CAT_API_PATH = 'http://172.30.0.86/api/cats'  # when deployed on cluster
-COLOR_ENC_API_PATH = 'http://54.72.183.142/api/encoding'   # when running locally
-# COLOR_ENC_API_PATH = 'http://172.30.1.89/api/encoding'  # when deployed on cluster
-VGG16_API_PATH = 'http://34.255.97.110/api/encoding'  # when running locally
-# VGG16_API_PATH = 'http://172.30.1.162/api/encoding'  # when deployed on cluster
+# CAT_API_PATH = 'http://34.245.0.175/api/cats'  # when running locally
+CAT_API_PATH = 'http://172.30.0.86/api/cats'  # when deployed on cluster
+# COLOR_ENC_API_PATH = 'http://54.72.183.142/api/encoding'   # when running locally
+COLOR_ENC_API_PATH = 'http://172.30.1.89/api/encoding'  # when deployed on cluster
+# VGG16_API_PATH = 'http://34.255.97.110/api/encoding'  # when running locally
+VGG16_API_PATH = 'http://172.30.1.162/api/encoding'  # when deployed on cluster
